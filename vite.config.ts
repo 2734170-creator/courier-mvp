e5979ca8199b6774788ec7fcf207c5add1ca2a54
2734170-creator <2734170@gmail.com>
2026-02-03 23:41:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Важно для корректной работы на GitHub Pages и Vercel
+  base: '/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false
   }
 });
