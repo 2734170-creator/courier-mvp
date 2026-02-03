99a3523e600578a51e9f5843535c40f6e0e43fbf
2734170-creator <2734170@gmail.com>
2026-02-04 00:10:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     sourcemap: false
   }
 });
